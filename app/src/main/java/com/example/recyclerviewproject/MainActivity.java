3783897346fd0434c677b348b53034e62025f117
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-04-30 08:55:40 +0200

    Moved activity code to fragment

--- app/src/main/java/com/example/recyclerviewproject/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewproject/MainActivity.java
@@ -1,70 +1,12 @@
 package com.example.recyclerviewproject;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText todoText;
-    private Button addButton;
-    private RecyclerView todoList;
-
-    private ArrayList<Todo>data;
-    private MyAdapter mAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        todoText = findViewById(R.id.todo_input);
-        addButton = findViewById(R.id.button_add_todo);
-        todoList = findViewById(R.id.todo_list);
-
-        prepareDummyData();
-
-       // RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
-
-        todoList.setLayoutManager(mLayoutManager);
-
-        mAdapter = new MyAdapter(data);
-        todoList.setAdapter(mAdapter);
-
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String task = todoText.getText().toString();
-
-                if(task.equals("")){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setMessage("Empty text!");
-                    builder.create().show();
-                }
-                else{
-                    Todo todo = new Todo (todoText.getText().toString());
-                    data.add(todo);
-                    mAdapter.notifyDataSetChanged();
-                    todoText.setText("");
-                }
-            }
-        });
-    }
-
-    private void prepareDummyData(){
-        data = new ArrayList<>();
-        data.add(new Todo("Todo 1"));
-        data.add(new Todo("Todo 2"));
-        data.add(new Todo("Todo 3"));
-        data.add(new Todo("Todo 4"));
     }
 }
--- app/src/main/java/com/example/recyclerviewproject/MainFragment.java
+++ app/src/main/java/com/example/recyclerviewproject/MainFragment.java
@@ -2,11 +2,22 @@ package com.example.recyclerviewproject;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainFragment extends Fragment {
+    private EditText textTodo;
+    private RecyclerView list;
+
+    private MyAdapter mAdapter;
+    private ArrayList<Todo> dataSet;
 
     public MainFragment() {
         // Required empty public constructor
@@ -24,7 +35,38 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        textTodo = view.findViewById(R.id.todo_input);
+        list = view.findViewById(R.id.todo_list);
+        RecyclerView.LayoutManager mLayourManager = new LinearLayoutManager(getContext());
+        list.setLayoutManager(mLayourManager);
+
+        dataSet = prepareDummyData();
+
+        mAdapter = new MyAdapter(dataSet);
+        list.setAdapter(mAdapter);
+        Button addTodoButton = view.findViewById(R.id.button_add_todo);
+        addTodoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataSet.add(new Todo(textTodo.getText().toString()));
+                mAdapter.notifyDataSetChanged();
+                textTodo.setText("");
+            }
+        });
+
+        return view;
+    }
+
+
+    private ArrayList<Todo> prepareDummyData() {
+        ArrayList<Todo> dataSet = new ArrayList<>();
+        dataSet.add(new Todo("Todo 1"));
+        dataSet.add(new Todo("Todo 2"));
+        dataSet.add(new Todo("Todo 3"));
+        dataSet.add(new Todo("Todo 4"));
+        return dataSet;
     }
 }
