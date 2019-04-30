0e2366cdd9043966d249c54c5b7465597fc03c59
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-04-30 09:27:53 +0200

    Adapted activity to keep fragment if initialised

--- app/src/main/java/com/example/recyclerviewproject/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewproject/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.recyclerviewproject;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -12,9 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        MainFragment mf = MainFragment.newInstance();
-        ft.add(R.id.list_fragment, mf);
-        ft.commit();
+        Fragment bf = fm.findFragmentById(R.id.list_fragment);
+        if (bf == null) {
+            FragmentTransaction ft = fm.beginTransaction();
+            MainFragment mf = MainFragment.newInstance();
+            ft.add(R.id.list_fragment, mf);
+            ft.commit();
+        }
+
     }
 }
