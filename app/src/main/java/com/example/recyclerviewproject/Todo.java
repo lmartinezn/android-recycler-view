dd193d6d0a9651799b92b12f1378f5979c1f8fe1
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-05-08 08:59:15 +0200

    Added entity information

--- app/src/main/java/com/example/recyclerviewproject/Todo.java
+++ app/src/main/java/com/example/recyclerviewproject/Todo.java
@@ -1,6 +1,14 @@
 package com.example.recyclerviewproject;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Todo {
+    @PrimaryKey(autoGenerate = true) //Id autoincrementat començant des de 1
     public int id;
+
+    @ColumnInfo(name="task") //Nom del camp a la base de dades, en cas de ser el mateix no cal
     public String task;
 }
