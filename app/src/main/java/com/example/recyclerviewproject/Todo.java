4f5cb93ee1745fcac3f02ba84203c6853304c314
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-05-08 08:57:31 +0200

    Converted Todo into Entity for Room

--- app/src/main/java/com/example/recyclerviewproject/Todo.java
+++ app/src/main/java/com/example/recyclerviewproject/Todo.java
@@ -1,25 +1,6 @@
 package com.example.recyclerviewproject;
 
-import java.util.Random;
-
-class Todo {
-    private int id;
-    private String task;
-
-    public Todo(String task){
-        this.task = task;
-        this.id = new Random().nextInt(99999);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTask() {
-        return task;
-    }
-
-    public void setTask(String task) {
-        this.task = task;
-    }
+public class Todo {
+    public int id;
+    public String task;
 }
